2c227c1a7f151d1e0a34a3ab3162110d9f3041cb
Atul Varma <varmaa@gmail.com>
2017-02-20 07:21:43 -0500

    Factor out download-form.tsx.

--- nodejs/src/renderer/app.tsx
+++ nodejs/src/renderer/app.tsx
@@ -1,72 +1,32 @@
 import React = require('react');
-import * as Redux from 'redux';
+import {Dispatch} from 'redux';
 import {connect} from 'react-redux';
 
-import * as actions from '../actions';
 import * as reduxState from '../state';
+import DownloadForm from './download-form';
 
 interface StateProps {}
 
-interface DispatchProps {
-  enqueueDownload: (url: string) => void;
-}
+interface DispatchProps {}
 
 interface OwnProps {}
 
-interface Props extends StateProps, DispatchProps, OwnProps {
-}
+type Props = StateProps & DispatchProps & OwnProps;
 
-interface State {
-  url: string;
-}
+interface State {}
 
 class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      url: '',
-    };
-  }
-
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    this.props.enqueueDownload(this.state.url);
-    this.setState({
-      url: '',
-    });
-  }
-
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      url: e.target.value,
-    });
-  }
-
   render() {
-    const id = 'url';
-
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor={id}>URL</label>
-        <input id={id} type="url" value={this.state.url}
-               onChange={this.handleChange} />
-        <button type="submit">Download</button>
-      </form>
+      <div>
+        <DownloadForm/>
+      </div>
     );
   }
 }
 
-function mapStateToProps(state: reduxState.State): StateProps {
-  return {};
-}
-
-function mapDispatchToProps(dispatch: Redux.Dispatch<reduxState.State>): DispatchProps {
-  return {
-    enqueueDownload: (url: string) => dispatch(actions.enqueueDownload(url))
-  };
-}
-
 export default connect<StateProps, DispatchProps, OwnProps>(
-  mapStateToProps,
-  mapDispatchToProps
+  (state: reduxState.State): StateProps => ({}),
+
+  (dispatch: Dispatch<reduxState.State>): DispatchProps => ({}),
 )(App);
